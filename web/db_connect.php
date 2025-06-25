22255b132ab745ef717ad8841ae205b355ac74c2
Niraj <149818536+Niraj0011@users.noreply.github.com>
2025-06-25 21:35:15 +0545

    Update db_connect.php

--- web/db_connect.php
+++ web/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 function db_connect() {
-    $conn = mysqli_connect("localhost", "root", "#nirajshwkt987", "project_db"); // Update password and database name
+    $conn = mysqli_connect("localhost", "root", "", ""); // Update password and database name
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error() . " (Error Code: " . mysqli_connect_errno() . ")");
     }
