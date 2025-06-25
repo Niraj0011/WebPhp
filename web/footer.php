dc5b2a26a6d881607d170512c72bfb2ccfc38b23
Niraj <149818536+Niraj0011@users.noreply.github.com>
2025-06-25 22:53:54 +0545

    Update footer.php

--- web/footer.php
+++ web/footer.php
@@ -3,6 +3,6 @@
    <?php echo "© " . date("Y") . " My Website"; ?>
     <p>Built By: <a href="https://github.com/Niraj0011">Niraj Shiwakoti</a> , Raman Baral, Sangam Gywali, Anuj Poudel</p>
   </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+ 
 </body>
 </html>
